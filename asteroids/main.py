d62bd065ded010c56d0e7d40f6423244163a96fb
Matthew Grimes <matt.grimz@gmail.com>
2025-08-21 03:21:45 -0600

    nightly: added circleshape and partial triangle

--- asteroids/main.py
+++ asteroids/main.py
@@ -4,25 +4,40 @@
 import pygame
 from constants import *
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.delta_time = 0
+        self.ms = 0
+        self.elapsed_time = 0
+
+    def test(self):
+        if self.elapsed_time >= 5:
+            print(f"Milliseconds per frame: {self.ms}")
+            self.elapsed_time = 0
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+            # Test
+            self.elapsed_time += self.delta_time
+            self.test()
+
+            self.screen.fill((0,0,0))
+            pygame.display.flip()
+            self.ms = self.clock.tick(60)
+            self.delta_time = self.ms / 1000
+            # self.delta_time = self.clock.tick(60) / 1000
+
 def main():
-
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-
-    pygame.init()
-
-    # print(pygame.display.get_driver())
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill((0,0,0))
-        pygame.display.flip()
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
     main()
